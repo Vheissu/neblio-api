0b90482d00501bd2aec24a163591244712901763
Dwayne <dwaynecharrington@gmail.com>
2021-02-12 10:25:02 +1000

    feat(routes): additional rpc calls

--- routes.ts
+++ routes.ts
@@ -11,19 +11,37 @@ export class NeblioRoutes {
 
         this.router.get(`${this.path}/getlatestblock`, this.getLatestBlock);
         this.router.get(`${this.path}/getblockbynumber/:blocknumber`, this.getBlockByNumber);
+        this.router.get(`${this.path}/getblockhash/:blocknumber`, this.getBlockHash);
+        this.router.get(`${this.path}/getblock/:hash`, this.getBlock);
     }
 
     public getLatestBlock = async (request: express.Request, response: express.Response) => {
         const rpcResponse = await this.rpcClient.request('getblockcount');
 
-        return rpcResponse.result;
+        return response.json(rpcResponse.result);
     }
 
     public getBlockByNumber = async (request: express.Request, response: express.Response) => {
         const blockNumber = request.params.blocknumber;
 
-        const rpcResponse = await this.rpcClient.request('getblockbynumber', [blockNumber, true]);
+        const rpcResponse = await this.rpcClient.request('getblockbynumber', [parseInt(blockNumber), true]);
 
-        return rpcResponse.result;
+        return response.json(rpcResponse.result);
+    }
+
+    public getBlockHash = async (request: express.Request, response: express.Response) => {
+        const blockNumber = request.params.blocknumber;
+
+        const rpcResponse = await this.rpcClient.request('getblockhash', [parseInt(blockNumber)]);
+
+        return response.json(rpcResponse.result);
+    }
+
+    public getBlock = async (request: express.Request, response: express.Response) => {
+        const blockHash = request.params.hash;
+
+        const rpcResponse = await this.rpcClient.request('getblock', [blockHash, true, true]);
+
+        return response.json(rpcResponse.result);
     }
 }
